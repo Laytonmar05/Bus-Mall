02c0ad029c3b18326eecaaf6997cb82c2619d46a
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-06-07 08:07:45 -0700

    Bus-Mall Final

--- script/script.js
+++ script/script.js
@@ -19,9 +19,9 @@ let PRODUCTS_ARRAY = [
   {HTMLid: 'sweep', imgURL: './images/sweep.png', totalViews: 0, totalVotes: 0},
   {HTMLid: 'tauntaun', imgURL: './images/tauntaun.jpg', totalViews: 0, totalVotes: 0},
   {HTMLid: 'unicorn', imgURL: './images/unicorn.jpg', totalViews: 0, totalVotes: 0},
-  {HTMLid: 'usb', imgURL: './images/usb.gif', totalViews: 0, totalVotes: 0},
   {HTMLid: 'waterCan', imgURL: './images/water-can.jpg', totalViews: 0, totalVotes: 0},
   {HTMLid: 'wineGlass', imgURL: './images/wine-glass.jpg', totalViews: 0, totalVotes: 0},
+
 ]
 
 let clicks = 0;
@@ -53,10 +53,7 @@ function handleClick(event) {
   console.log('I was clicked, and my id is: ', event.target.id)
   let imageid = event.target.id;
 
-  // when the parent container is clicked, how do i know which image it is ???
-  // we have to compare the id's
-  // if then else statement.. compare parentId to image id somehow.. 
-  // if parentid === 0, 1, 2 - array[0], or array[1], array[2]
+  // count the total votes/clicks
   for (let i=0; i < PRODUCTS_ARRAY.length; i++) {
     if ( imageid === PRODUCTS_ARRAY[i].HTMLid) {
       PRODUCTS_ARRAY[i].totalVotes++
@@ -64,13 +61,129 @@ function handleClick(event) {
     }
   }
 
+  // we need a conditional statement
+  if (clicks !== 25 ) {
+      // remove the first 3 images from the array
+      for(let i=0; i<3; i++) {
+
+        // generate a random number between 0 and x
+        let random =  Math.floor(Math.random() * PRODUCTS_ARRAY.length -1 );
+        
+        let item = PRODUCTS_ARRAY.shift();
+        PRODUCTS_ARRAY.splice(PRODUCTS_ARRAY.length -1 , 0, item  );
+      }
+
+      // remove it from the index.html (remove it from the DOM)
+      for (let i=0; i<3; i++) {
+        let parent = document.getElementById(`img${i}Container`);
+        parent.removeChild(parent.lastChild);
+      }
+      console.log('clicks: ', clicks);
+
+      renderImages();
+
+  }else {
+
+      let divs = document.getElementsByTagName('div');
+      for (let i=1; i<divs.length - 1; i++) {
+        divs[i].removeEventListener('click', handleClick);
+      }
+      console.log('you have reached 25 clicks')
+
+      // render the results by running a function()
+      renderResults();
+
+  }
+
+}
+
+function renderResults() {
+
+  // console.log('displaying results');
+  // I must target the parent, which in my case is..
+  let resultSection = document.getElementById('resultSection');
+  let div = document.createElement('div');
+  div.setAttribute('id', 'result');
+  resultSection.appendChild(div);
+
+  let h3 = document.createElement('h3');
+  h3.textContent = 'Results: ';
+  div.appendChild(h3);
+
+  let ol = document.createElement('ol');
+  div.appendChild(ol);
+
+  // PRODUCTS_ARRAY.sort( function(a, b) {
+  //   return b.totalVotes - a.totalVotes;
+  // });
+
+  for (let i=0; i < PRODUCTS_ARRAY.length; i++) {
+    // display all of the results;
+    let listItem = document.createElement('li');
+    listItem.textContent= `${PRODUCTS_ARRAY[i].totalVotes} for ${PRODUCTS_ARRAY[i].HTMLid}`;
+    ol.appendChild(listItem);
+  }
+
+  renderChart();
+
+}
+
+function renderChart() {
+  console.log('renderChart was called')
+  const barData = {
+    type: 'bar',
+    data: {
+      labels : [],
+      datasets : [
+        {
+          data: [],
+          backgroundColor: 'rgb(64, 211, 191',
+          borderColor: 'rbb(46, 146, 133',
+          pointBackgroundColor: 'rgb(46, 135, 100',
+        }
+      ]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          maxBarThickness: 30,
+        }],
+        yAxes: [{
+          gridLines: {
+            offsetGridLines: false,
+          },
+          ticks: {stepSize: 1},
+          maintainAspectRatio: false,
+        }]
+      },
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Final Vote Data'
+      }
+    }
+  };
+
+  let container = document.getElementById('graph');
+
+  let canvas = document.createElement('canvas');
+  let ctx = canvas.getContext('2d');
+  container.appendChild(canvas);
+
+  for ( let i=0; i< PRODUCTS_ARRAY.length; i++) {
+    barData.data.labels.push(PRODUCTS_ARRAY[i].HTMLid)
+    barData.data.datasets[0]['data'].push(PRODUCTS_ARRAY[i].totalVotes);
+  }
+
+  new Chart(ctx, barData);
+
 }
 
 
 //This is our entry point
 (function startApp() {
-
-  alert('The Poll has started');
 
   for(let i=0; i < 3; i++) {
     let imgContainer = document.getElementById(`img${i}Container`);
